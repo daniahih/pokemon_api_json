96d658ba1541d0d8e8ff84f6082a77b3b680a44f
Dania Hih <73383276+daniahih@users.noreply.github.com>
2025-05-13 12:06:26 +0300

    added .env file

--- constants.py
+++ constants.py
@@ -3,6 +3,3 @@ constants.py
 Application-wide constants for the Pokémon CLI app.
 """
 
-API_BASE_URL = "https://pokeapi.co/api/v2"
-POKEMON_LIST_ENDPOINT = f"{API_BASE_URL}/pokemon?limit=100000&offset=0"
-DB_PATH = "pokemon_db.json"
--- try.py
+++ try.py
@@ -5,7 +5,7 @@ POKEMON_LIST_ENDPOINT = f"{API_BASE_URL}/pokemon?limit=100000&offset=0"
 resp = requests.get(POKEMON_LIST_ENDPOINT)
 print(f"resp :{resp}")
 response = resp.json().get('results',[])
-print(f'response {response}')
+print(response)
 # Using dict.get(key, default) is the idiomatic way to look up a key and supply a fallback if the key isn’t present.
 
 # Here, if for some reason the API returns an object without "results", data.get("results", []) will give you an empty list ([]) instead of raising a KeyError.
